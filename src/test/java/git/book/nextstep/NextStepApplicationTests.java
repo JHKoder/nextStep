d7065175ddf613e85971e4d3e99b9b078326d68d
kang's <jeonghun.kang.dev@gmail.com>
2022-12-23 23:42:20 +0900

    Initial commit

---
+++ src/test/java/git/book/nextstep/NextStepApplicationTests.java
@@ -0,0 +1,13 @@
+package git.book.nextstep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextStepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
